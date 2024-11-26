06b66ea8a8522ea317ea497f7c7f99fe9c15d53a
m-rezk-meem <m.Rezk@meemdev.com>
2024-11-26 12:27:07 +0200

    fix typo

--- lib/liveblocks.ts
+++ lib/liveblocks.ts
@@ -1,9 +1,9 @@
 import { Liveblocks } from '@liveblocks/node';
 
-const key = process.env.LIVEBLOCKS_KRY;
+const key = process.env.LIVEBLOCKS_KEY;
 
 if (!key) {
-  throw new Error('LIVEBLOCKS_KRY is not defined');
+  throw new Error('LIVEBLOCKS_KEY is not defined');
 }
 
 const liveblocks = new Liveblocks({
